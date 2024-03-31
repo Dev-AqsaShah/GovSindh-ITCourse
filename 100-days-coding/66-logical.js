1bb342299d7c3810d89e79dc46f3fc3943ee532e
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-03-31 21:44:45 +0500

    Add files via upload

---
+++ 100-days-coding/66-logical.js
@@ -0,0 +1,8 @@
+// this function sees if both input are true
+function checkBothTrue(val1, val2) {
+    // only says true if both val1 and val2 are true 
+    return val1 && val2;
+}
+// trying it with true and false 
+console.log(checkBothTrue(true, false)); // shows false 
+// it checks two things, the answer is false.
---
+++ 100-days-coding/66-logical.ts
@@ -0,0 +1,11 @@
+// this function sees if both input are true
+function checkBothTrue(val1 : boolean, val2: boolean) : boolean{
+
+    // only says true if both val1 and val2 are true 
+    return val1 && val2;
+}
+
+// trying it with true and false 
+console.log(checkBothTrue(true,false)); // shows false 
+
+// it checks two things, the answer is false.
