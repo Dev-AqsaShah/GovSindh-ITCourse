4e11b399c682b54fc5f5ed67daf9d73b4dd26106
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-04-28 10:37:17 +0500

    Add files via upload

---
+++ 100-days-coding/day-40.js
@@ -0,0 +1,21 @@
+//question num-118
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+//question-num-119
+var count = 0;
+while (count < 5) {
+    console.log("Hello, World!");
+    count++;
+}
+//question-num-120
+var favoriteMovies = [
+    "Inception",
+    "The Matrix",
+    "Interstellar",
+    "The prestige",
+];
+for (var _i = 0, favoriteMovies_1 = favoriteMovies; _i < favoriteMovies_1.length; _i++) {
+    var movie = favoriteMovies_1[_i];
+    console.log(movie);
+}
