3cf1ffd9015e4ca9c93c8b2d4b52629a098d391d
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-05-12 12:53:46 +0500

    Add files via upload

---
+++ 100-days-coding/day-47/Q-140/index.js
@@ -0,0 +1,4 @@
+//Attemptig to use a reserved word as a variale name.
+// let if = 5; // this line would cause a sytax error.
+console.log("using a reserved word as a variable name causes a sytax error in javaScript.");
+// its importat to avoid using reserved words as idetifiers to prevent these errors.
