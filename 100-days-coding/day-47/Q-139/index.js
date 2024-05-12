2e30f7857dd82a767707dbc3b6ae4f4db92a734d
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-05-12 12:46:50 +0500

    Add files via upload

---
+++ 100-days-coding/day-47/Q-139/index.js
@@ -0,0 +1,7 @@
+var count = 5;
+if (count > 0) {
+    console.log("Count is greater than 0.");
+}
+function add(a, b) {
+    return a + b;
+}
---
+++ 100-days-coding/day-47/Q-139/index.ts
@@ -0,0 +1,9 @@
+let count = 5;
+
+if (count > 0) {
+    console.log("Count is greater than 0.");
+}
+
+function add(a, b) {
+    return a + b;
+}
