987ae091b54ff93a772a972ab3107c93164f122f
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-05-21 11:22:12 +0500

    Add files via upload

---
+++ 100-days-coding/day-50/Q-148/index.js
@@ -0,0 +1,5 @@
+//Uses setTimeout to llog  message after  a 2-second delay
+setTimeout(function () {
+    console.log("This message is shown after a 2-second delay.");
+}, 5000);
+//this showcases how to use setTimeout to delay action in your code.
---
+++ 100-days-coding/day-50/Q-148/index.ts
@@ -0,0 +1,5 @@
+//Uses setTimeout to llog  message after  a 2-second delay
+setTimeout(() => {
+    console.log("This message is shown after a 2-second delay.");
+}, 2000);
+//this showcases how to use setTimeout to delay action in your code.
