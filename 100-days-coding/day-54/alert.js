d5cd8518fc59ea9107886b71947f9865e430a5b2
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-05-25 20:13:19 +0500

    Add files via upload

---
+++ 100-days-coding/day-54/alert.js
@@ -0,0 +1,15 @@
+let admin, name; // can declare two variables at once
+
+name = "aqsa";
+
+admin = name;
+
+alert( admin ); // "aqsa"
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase?
+
+const AGE = someCode(BIRTHDAY); // make age uppercase?
