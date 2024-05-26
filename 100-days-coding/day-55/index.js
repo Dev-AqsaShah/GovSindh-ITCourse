a56e314c5aac8e8537bd196bd5d3eccbf3c1b8b4
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-05-26 13:32:38 +0500

    Add files via upload

---
+++ 100-days-coding/day-55/index.js
@@ -0,0 +1,10 @@
+let name = "aqsa";
+
+// the expression is a number 1
+alert( `hello ${1}` ); // hello 1
+
+// the expression is a string "name"
+alert( `hello ${"name"}` ); // hello name
+
+// the expression is a variable, embed it
+alert( `hello ${name}` ); // hello aqsa
