8944e856389e0a3e4139f5207bc504d0a424c18b
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-05-28 07:43:43 +0500

    updated

--- 100-days-coding/day-57/index.ts
+++ 100-days-coding/day-57/index.ts
@@ -5,3 +5,16 @@ undefined == null → true
 undefined === null → false
 null == "\n0\n" → false
 null === +"\n0\n" → false
+
+
+// part-2
+
+// Yes, it will.
+
+// Any string except an empty one (and "0" is not empty) becomes true in the logical context.
+
+// We can run and check:
+
+if ("0") {
+  alert( 'Hello' );
+}
