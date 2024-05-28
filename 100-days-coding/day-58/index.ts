472da98b5aa46e57034e096db38310e8f706ec60
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-05-28 18:17:25 +0500

    updated

--- 100-days-coding/day-58/index.ts
+++ 100-days-coding/day-58/index.ts
@@ -1,1 +1,27 @@
+alert( null || 2 || undefined );
 
+alert( alert(1) || 2 || alert(3) );
+
+alert(1 && null && 2);
+
+alert( alert(1) && alert(2) );
+
+alert( null || 2 && 3 || 4 );
+
+if (-1 || 0) alert( 'first' );
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' );
+
+// Runs.
+// The result of -1 || 0 = -1, truthy
+if (-1 || 0) alert( 'first' );
+
+// Doesn't run
+// -1 && 0 = 0, falsy
+if (-1 && 0) alert( 'second' );
+
+// Executes
+// Operator && has a higher precedence than ||
+// so -1 && 1 executes first, giving us the chain:
+// null || -1 && 1  ->  null || 1  ->  1
+if (null || -1 && 1) alert( 'third' );
