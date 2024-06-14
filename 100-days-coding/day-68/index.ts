4494584365e4e9db11bc2a8e03dfa6369f63a341
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-06-14 13:31:14 +0500

    Add files via upload

---
+++ 100-days-coding/day-68/index.ts
@@ -0,0 +1,18 @@
+// tuples
+
+// definig a tuple type 
+let article: readonly [number, string, boolean] = [11, "Title one", true];
+
+// assigning a new tuple value to the article  variable
+article = [12, "Title Two", false];
+
+// trying to push a new element into a tuple (will result in an error)
+// article.push(100);
+
+console.log(article);
+
+// destructuring the tuple into individual variables
+const [id, title, published] = article;
+console.log(id);    
+console.log(title);
+console.log(published);
