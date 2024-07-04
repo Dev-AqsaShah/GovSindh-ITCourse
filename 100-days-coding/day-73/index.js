3153acf99fa1a7e2f207e1ca41b7c03d5e4f1267
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-07-04 15:13:30 +0500

    Add files via upload

---
+++ 100-days-coding/day-73/index.js
@@ -0,0 +1,4 @@
+// TYPE ASSERTIONS
+var data = "1000";
+// type assertion to treat "data" as a string
+console.log(data.repeat(3));
--- 100-days-coding/day-73/index.ts
+++ 100-days-coding/day-73/index.ts
@@ -1,1 +1,6 @@
+// TYPE ASSERTIONS
 
+let data: any = "1000";
+
+// type assertion to treat "data" as a string
+console.log((data as string).repeat(3));
