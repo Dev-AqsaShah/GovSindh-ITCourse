d1bafce3a1e52a125c40f22e005e31a8ad58c471
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-07-04 15:11:16 +0500

    Add files via upload

---
+++ 100-days-coding/day-74/index.js
@@ -0,0 +1,11 @@
+// DEBUGING
+var age = 22;
+if (age > 20) {
+    console.log("pass");
+}
+else if (age < 20) {
+    console.log("return");
+}
+else {
+    console.log("reboot");
+}
---
+++ 100-days-coding/day-74/index.ts
@@ -0,0 +1,10 @@
+// DEBUGING
+
+let age: number = 22;
+if (age > 20) {
+    console.log("pass");
+} else if (age < 20) {
+    console.log("return");
+} else {
+    console.log("reboot");
+}
